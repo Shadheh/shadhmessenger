5035497ca8321715f36e6ec05e5b155ea926ec22
shadhey <shadhitmuhammed8111@gmail.com>
2025-06-05 03:51:29 -0700

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
-console.log('Chat app loaded');
+
+const socket = io();
+function send() {
+    const msg = document.getElementById('msg').value;
+    socket.emit('message', msg);
+    const div = document.createElement('div');
+    div.textContent = "You: " + msg;
+    document.getElementById('chat').appendChild(div);
+}
+socket.on('message', (msg) => {
+    const div = document.createElement('div');
+    div.textContent = "Friend: " + msg;
+    document.getElementById('chat').appendChild(div);
+});
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Express server placeholder
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 10000;
+http.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
